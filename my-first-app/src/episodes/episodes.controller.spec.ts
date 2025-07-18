98fbbf750ae9ab4609ed7cb8af1ab47a59b858ed
ShifatJahanShifa <bsse1301@iit.du.ac.bd>
2025-07-18 07:01:55 +0600

    learn: controller, provider

--- my-first-app/src/episodes/episodes.controller.spec.ts
+++ my-first-app/src/episodes/episodes.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EpisodesController } from './episodes.controller';
+import { EpisodesService } from './episodes.service';
+import { ConfigModule } from 'src/config/config.module';
+import { ConfigService } from 'src/config/config.service';
 
 describe('EpisodesController', () => {
   let controller: EpisodesController;
@@ -7,6 +10,8 @@ describe('EpisodesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EpisodesController],
+      providers: [EpisodesService, ConfigService],
+      imports: [ConfigModule]
     }).compile();
 
     controller = module.get<EpisodesController>(EpisodesController);
--- my-first-app/src/episodes/episodes.controller.ts
+++ my-first-app/src/episodes/episodes.controller.ts
@@ -1,32 +1,39 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { get } from 'http';
+import { EpisodesService } from './episodes.service';
+import { CreateEpisodeDTO } from './dto/create-episode.dto';
+import { ConfigService } from 'src/config/config.service';
 
 // this string is a root path
 @Controller('episodes')
 export class EpisodesController {
 
+    constructor(private episodeService: EpisodesService, 
+        private configService: ConfigService,
+    ) {}
+
     @Get()
     findAll(@Query('sort') sort: 'asc' | 'desc' = 'desc') 
     {
         console.log(sort)
-        return "all controllers"
+        return this.episodeService.findAll(sort)
     }
 
     // episodes/featured
     @Get("featured")
     findFeatured() {
-        return "featured episodes"
+        return this.episodeService.findFeatured()
     }
 
     @Get(':id')
     findOne(@Param() id: string) 
     {
-        return 'one episode'
+        return this.episodeService.findOne(id)
     }
 
     // string can be used in post decorator. which indicates the url parameters
     @Post()
-    create(@Body() input: any) {
-        return "new episode"
+    create(@Body() input: CreateEpisodeDTO) {
+        return this.episodeService.create(input)
     }
 }
